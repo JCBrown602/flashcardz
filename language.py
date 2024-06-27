14297e45f452c56f5cd62dcb16eec0166b172a34
JCBrown <3117301+JCBrown602@users.noreply.github.com>
2024-06-27 11:14:16 -0500

    Initial.

---
+++ language.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from random import randint
+
+root = Tk()
+root.title('Spanish Language Cards')
+photo = PhotoImage(file = 'mex-flag_2.png')
+root.iconphoto(False, photo)
+root.geometry("550x410")
+
+words = [
+	(("Hola"), ("Hello")),			#1
+	(("Adios"), ("Goodbye")),
+	(("Por favor"), ("Please")),
+	(("Gracias"), ("Thank you")),
+	(("Lo siento"), ("Sorry")),		#5
+	(("Salud"), ("Bless you")),
+	(("Si"), ("Yes")),
+	(("No"), ("No")),
+	(("Quien?"), ("Who?")),
+	(("Que?"), ("What?")),			#10
+	(("Por que?"), ("Why?")),
+	(("Donde"), ("Where?"))
+]
+
+# Get a count of the words
+count = len(words)
+
+def next():
+	# Random selection
+	random_word = randint(0, count-1)
+	# Update label
+	spanish_word.config(text=words[random_word][0])
+
+spanish_word = Label(root, text="", font=("Helvetica", 30))
+spanish_word.pack(pady=50)
+
+answer_label = Label(root, text="")
+answer_label.pack(pady=20)
+
+my_entry = Entry(root, font=("Helvetica", 18))
+my_entry.pack(pady=20)
+
+# Buttons
+button_frame = Frame(root)
+button_frame.pack(pady=20)
+
+answer_button = Button(button_frame, text="Answer")
+answer_button.grid(row=0, column=0, padx=20)
+
+next_button = Button(button_frame, text="Next", command=next)
+next_button.grid(row=0, column=1)
+
+hint_button = Button(button_frame, text="Hint")
+hint_button.grid(row=0, column=2, padx=20)
+
+# Hint Label
+hint_label = Label(root, text="")
+hint_label.pack(pady=20)
+
+next()
+
+root.mainloop()
